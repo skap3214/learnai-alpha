dcb965271e077aa2270667eeb5c8f8d46b8074e6
skap3214 <kapadiasoami@gmail.com>
2023-04-16 18:35:45 -0400

    added backend frontend integration

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import main as back
-files = st.file_uploader("Upload your PDF files", help="We currently only support .pdf files.",accept_multiple_files=True)
+import os
+
+pdf = st.file_uploader("Upload your PDF files here", help="We currently only support .pdf files.",accept_multiple_files=True)
+if pdf:
+    merged_output = 'output.pdf'
+    docsearch, chain = back.preprocess_text(pdf, merged_output)
+    st.write(back.pdf_summary(docsearch, chain))  # Print part_1 (summary)
+    st.write(back.pdf_questions_answers(docsearch, chain))  # Print part_2 (questions and answers)
+    os.remove(merged_output)
 
 
