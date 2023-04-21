34e50e67590aa8449074a6f204821b409592dee9
Advait Paliwal <advaitspaliwal@gmail.com>
2023-04-21 17:20:51 -0400

    streamlit

--- app.py
+++ app.py
@@ -1,42 +1,72 @@
 import streamlit as st
 
-def main():
-    st.title("Learn.ai")
 
-    # Input for YouTube video URL
-    video_url = st.text_input("Enter YouTube video URL:")
-
-    col1, col2 = st.columns(2)
-
-    # col1
+def display_video(col1, video_url):
     if video_url:
+        st.session_state.video_url = video_url
         with col1:
             st.video(video_url)
 
-    # col2
+
+def notes_tab(tab):
+    with tab:
+        st.header("Notes")
+        st.text_area("Take notes here:")
+
+
+def mcq_tab(tab):
+    with tab:
+        st.header("MCQ")
+        st.subheader(f"Question {1}")
+        st.radio("Choices", ["A", "B", "C", "D"])
+        if st.button("Submit"):
+            st.success("Correct!")
+
+
+def chat_tab(tab):
+    with tab:
+        st.header("Chat")
+
+        if "conversation_history" not in st.session_state:
+            st.session_state.conversation_history = []
+
+        user_input = st.text_input("Enter your message:")
+
+        with st.expander("Chat History", expanded=True):
+            conversation = st.container()
+            conversation.markdown(f'<span style="color:green">**Lenny:**</span> Hi! How can I help you?',
+                                  unsafe_allow_html=True)
+
+            for message in st.session_state.conversation_history:
+                conversation.markdown(message, unsafe_allow_html=True)
+            if user_input:
+                user_message = f'<span style="color:blue">**User:**</span> {user_input}'
+                conversation.markdown(user_message, unsafe_allow_html=True)
+                st.session_state.conversation_history.append(user_message)
+                lenny_response = "Ok"
+                lenny_message = f'<span style="color:green">**Lenny:**</span> {lenny_response}'
+                conversation.markdown(lenny_message, unsafe_allow_html=True)
+                st.session_state.conversation_history.append(lenny_message)
+
+
+def main():
+    st.title("Learn.ai")
+
+    if "video_url" not in st.session_state:
+        st.session_state.video_url = ""
+
+    video_url = st.text_input("Enter YouTube video URL:", value=st.session_state.video_url)
+
+    col1, col2 = st.columns([2, 1])
+
+    display_video(col1, video_url)
+
     with col2:
-        button_col1, button_col2, button_col3 = st.columns(3)
-        with button_col1:
-            notes_button = st.button("Notes")
-        with button_col2:
-            mcq_button = st.button("MCQ")
-        with button_col3:
-            chatbot_button = st.button("Chatbot")
+        tab1, tab2, tab3 = st.tabs(["Notes", "MCQ", "Chat"])
+        notes_tab(tab1)
+        mcq_tab(tab2)
+        chat_tab(tab3)
 
-        if notes_button:
-            # Display notes section
-            st.header("Notes")
-            st.text_area("Take notes here:")
-
-        elif mcq_button:
-            # Display MCQ section
-            st.header("MCQ")
-            # Add sample MCQs or user-defined MCQs
-
-        elif chatbot_button:
-            # Display chatbot section
-            st.header("Chatbot")
-            # Add your chatbot implementation
 
 if __name__ == "__main__":
     main()
