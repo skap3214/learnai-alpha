c65bc17d40e90f9719aa5d537c1cc5ea0bf4435f
skap3214 <kapadiasoami@gmail.com>
2023-04-22 13:38:15 -0400

    updated text classes

--- text_conversions.py
+++ text_conversions.py
@@ -3,31 +3,65 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
-
+import json
 class Converter:
     '''
     converts to everything from text. To call a specific method, 
     the method name will be whateer you want to convert the text to.
     e.g:
     If you want to convert text to mcq, then you will call mcq() method.
+
+    Supported text conversion methods:
+    - mcq->json
+    - chatbot
     '''
-    def __init__(self,text):
+    API_KEY = "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"
+
+    def __init__(self, text : str):
+        '''
+        Constructor which takes in text as input.
+        :param text: text to be converted.
+        :type text: str
+        
+        :Example:
+        >>> text = "This is a sample text to be converted"
+        >>> converter = Converter(text)
+        >>> converter.mcq()
+        >>> converter.chatbot()
+        >>> converter.json()
+        
+        '''
         self.text = text
     
     def mcq(self):
         '''
         converts to mcq(in dictionary format)
         '''
-        pass
-    
-    def chatbot(self,question):
+        question = "Create a multiple choice question and answer sequence using the whole text as context. Create as many relevant questions as possible"
+        convert_json = '''
+        I will give you a series of Multiple Choice Questions, with their options and answers. I need to convert all of it to a json object. This is an example of how I want the json object to look like:
+        {
+            "1" : {
+                "question": "q",
+                "A": "answer a",
+                "B": "answer b",
+                "C": "answer c",
+                "D": "answer d",
+                "correct":"A/B/C/D"
+                
+            },
+            "2" : {
+                "question": "q",
+                "A": "answer a",
+                "B": "answer b",
+                "C": "answer c",
+                "D": "answer d",
+                "correct":"A/B/C/D"
+                
+            }
+        }
+        Strictly output ONLY the json object.
         '''
-        converts to chatbot(Q/A)
-        - does not retain context
-        - you need to call chatbot everytime you want to ask a question
-        '''
-        OPENAI_API_KEY = "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"
         text_splitter = CharacterTextSplitter(
             separator="\n",
             chunk_size=1000,
@@ -36,9 +70,37 @@ class Converter:
         )
         texts = text_splitter.split_text(self.text)
 
-        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+        embeddings = OpenAIEmbeddings(openai_api_key=self.API_KEY)
         docsearch = FAISS.from_texts(texts, embeddings)
-        llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY)
+        llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=self.API_KEY, temperature=0)
+
+        chain = load_qa_chain(llm=llm, chain_type="stuff")
+        docs = docsearch.similarity_search(question)
+        result = chain.run(input_documents = docs, question = question)
+
+                
+        llm = OpenAI(temperature=0, openai_api_key=self.API_KEY, model_name="gpt-3.5-turbo")
+        to_json = llm(convert_json+"\nThis is the result:\n"+result)
+        output = json.loads(to_json.strip())
+        return output
+    
+    def chatbot(self,question):
+        '''
+        converts to chatbot(Q/A)
+        - does not retain context
+        - you need to call chatbot everytime you want to ask a question
+        '''
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+        )
+        texts = text_splitter.split_text(self.text)
+
+        embeddings = OpenAIEmbeddings(openai_api_key=self.API_KEY)
+        docsearch = FAISS.from_texts(texts, embeddings)
+        llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=self.API_KEY)
         chain = load_qa_chain(llm=llm, chain_type="stuff")
         docs = docsearch.similarity_search(question)
         result = chain.run(input_documents = docs, question = question)
@@ -50,11 +112,32 @@ class Converter:
         '''
         pass
     
-    def summary(self):
+    def summary(self,as_list = False):
         '''
         converts text to a summary
+        as_list: If you want each of the points in the summary as a list.
+
+        #TODO: CONVERT SUMMARY TO LIST
+        #TODO: CONVERT LIST TO A FORMATTED STRING FOR OUTPUT
+
+        currently outputs a summary as a string in a python list format
         '''
-        pass
+        prompt = "Create a summary in a Python list format"
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+        )
+        texts = text_splitter.split_text(self.text)
+
+        embeddings = OpenAIEmbeddings(openai_api_key=self.API_KEY)
+        docsearch = FAISS.from_texts(texts, embeddings)
+        llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=self.API_KEY)
+        chain = load_qa_chain(llm=llm, chain_type="stuff")
+        docs = docsearch.similarity_search(prompt)
+        result = chain.run(input_documents = docs, question = prompt)
+        return result
 
     def cheat_sheet(self):
         '''
--- text_grabber.py
+++ text_grabber.py
@@ -1,74 +1,88 @@
 import os
-import re
-from youtube_transcript_api import YouTubeTranscriptApi
-#import whisper_timestamped as whisper
+from PyPDF2 import PdfReader, PdfMerger
+import whisper_timestamped as whisper
 from pytube import YouTube
-
+import ffmpeg
 class ToText:
     '''
     to convert a type of file to text, call the type of file as the method name.
     e.g: If you want to convert a youtube video to text:
     ToText.youtube
+
+    Supported file types:
+    - youtube
     '''
+    API_KEY = "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"
     #create a method called youtube which takes in a URL as a parameter
-    # def youtube(self,video_URL,timestamp_yes = False, as_string = True):
-    #     '''
-    #     timestamp_yes:
-    #     this param is set to False as default. If you set it as True, the method will return a tuple of two elements(transcript, timestamps)
-    #
-    #     as_string:
-    #     this param is set to True as defualt. If you set it as False, the method will return a list of words. otherwise it returns the transcript as a string.
-    #     '''
-    #     #Use pytube to download the audio from the URL
-    #     video = YouTube(video_URL)
-    #     audio = video.streams.filter(only_audio=True).first()
-    #     new_file = "audio" + '.mp3'
-    #     audio.download(filename=new_file)
-    #     os.rename(new_file, "audio.mp3")
-    #     # Load the audio file
-    #     audio = whisper.load_audio("audio.mp3")
-    #
-    #     # Load the pre-trained model
-    #     model = whisper.load_model("tiny", device="cpu")
-    #
-    #     # Transcribe the audio
-    #     result = whisper.transcribe(model, audio, language="en")
-    #
-    #
-    #     # Save the result to a JSON file
-    #     timestamps = []
-    #     transcript = []
-    #     segments = result['segments']
-    #     for dict in segments:
-    #         words = dict['words']
-    #         for dict_1 in words:
-    #             word = dict_1['text']
-    #             start = dict_1['start']
-    #             end = dict_1['end']
-    #             transcript.append(word)
-    #             timestamps.append((start,end))
-    #     os.remove("audio.mp3")
-    #     if as_string:
-    #         transcript = " ".join(transcript)
-    #     if timestamp_yes:
-    #         return transcript, timestamps
-    #     else:
-    #         return transcript
+    def youtube(self,video_URL,timestamp_yes = False, as_string = True):
+        '''
+        timestamp_yes:
+        this param is set to False as default. If you set it as True, the method will return a tuple of two elements(transcript, timestamps)
 
-    def yt_transcript(self, url):
-        """
-        Takes a URL to a YouTube video and returns the transcript text.
-        """
-        pattern = r"(?:/|%3D|v=)([0-9A-Za-z_-]{11})(?:[%#?&]|$)"
-        match = re.search(pattern, url)
-        if match:
-            video_id = match.group(1)
+        as_string:
+        this param is set to True as defualt. If you set it as False, the method will return a list of words. otherwise it returns the transcript as a string.
+        '''
+        output_file = "audio.mp3"
+        # Get the YouTube object
+        yt = YouTube(video_URL)
+
+        # Download the highest quality audio stream
+        audio_stream = yt.streams.filter(only_audio=True, subtype='mp4').first()
+
+        # Download the audio file
+        audio_file = audio_stream.download(filename='temp_audio')
+
+        # Convert the downloaded audio file to MP3
+        input_audio = ffmpeg.input(audio_file)
+        ffmpeg.output(input_audio, output_file, format='mp3').run()
+        # Load the audio file
+        audio = whisper.load_audio("audio.mp3")
+
+        # Load the pre-trained model
+        model = whisper.load_model("tiny", device="cpu")
+
+        # Transcribe the audio
+        result = whisper.transcribe(model, audio, language="en")
+
+
+        # Save the result to a JSON file
+        timestamps = []
+        transcript = []
+        segments = result['segments']
+        for dict in segments:
+            words = dict['words']
+            for dict_1 in words:
+                word = dict_1['text']
+                start = dict_1['start']
+                end = dict_1['end']
+                transcript.append(word)
+                timestamps.append((start,end))
+        os.remove("audio.mp3")
+        os.remove(audio_file)
+        if as_string:
+            transcript = " ".join(transcript)
+        if timestamp_yes:
+            return transcript, timestamps
         else:
-            print("Error: Invalid YouTube URL.")
-            return False
-
-        transcript = YouTubeTranscriptApi.get_transcript(str(video_id).strip(),
-                                                         languages=["en", "en-GB", "en-US"])
-        corpus = " ".join([t["text"] for t in transcript])
-        return corpus
+            return transcript
+    
+    def pdf(self,pdf_list):
+        '''
+        pdf_list:
+        this param is a list of pdf files.
+        #TODO: NOT TESTED
+        '''
+        merger = PdfMerger()
+        
+        for pdf in pdf_list:
+            merger.append(pdf)
+        merger.write("merged.pdf")
+        merged_pdf = "merged.pdf"
+        reader = PdfReader(merged_pdf)
+        raw_text = ''
+        for i, page in enumerate(reader.pages):
+            text = page.extract_text()
+            if text:
+                raw_text += text
+        return raw_text
 
